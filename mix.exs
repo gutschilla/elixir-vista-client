eec16629d32641f4302faa044b07bada381f9499
Martin Gutsch <gutsch@zwoelf.net>
2019-10-22 17:24:06 +0200

    bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VistaClient.MixProject do
   def project do
     [
       app: :vista_client,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
